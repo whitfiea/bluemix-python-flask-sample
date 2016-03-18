74a15613a760a56a4a769193e285cc9522e83f9a
Andrew Whitfield <whitfiea@Andrews-MacBook-Pro.local>
2016-03-18 14:38:23 +0000

    Update to use PORT env property and python 2.7.11

--- welcome.py
+++ welcome.py
@@ -11,6 +11,6 @@ def Welcome():
 def WelcomeToMyapp():
     return 'Welcome again to my app running on Bluemix!'
 
-port = os.getenv('VCAP_APP_PORT', '5000')
+port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(port))
